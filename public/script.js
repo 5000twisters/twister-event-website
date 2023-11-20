ed076567a5f8d489d4a39422231e84cef7127e7b
5000twisters <151400731+5000twisters@users.noreply.github.com>
2023-11-20 22:15:07 +0000

    commit

--- public/script.js
+++ public/script.js
@@ -1,55 +1,14 @@
-form.addEventListener('submit', function(event) {
-    // prevent the default behavior of the form
-    event.preventDefault();
-    // get the values of the input fields
-    const name = nameInput.value;
-    const email = emailInput.value;
-    const option = dropdown.value;
-    const frequency = checkbox1.checked;
-    const topic1 = checkbox2.checked;
-    const topic2 = checkbox3.checked;
-    // validate the email address
-    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    const isValidEmail = emailPattern.test(email);
-    // if the email is not valid, show an alert message and stop the function
-    if (!isValidEmail) {
-      alert('Please enter a valid email address.');
-      return;
-    }
-    // send the data to the database
-    fetch('https://api.mongodb.com/v1/databases/mydatabase/collections/mycollection', {
-      method: 'POST',
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        option: option,
-        frequency: frequency,
-        topic1: topic1,
-        topic2: topic2
-      })
-    }).then(function(response) {
-      // parse the response body as a JSON object
-      return response.json();
-    }).then(function(data) {
-      // check the status code of the request
-      if (data.status === 201) {
-        // the document was created successfully
-        // update the counter
-        const currentCount = parseInt(counter.textContent);
-        const newCount = currentCount + 1;
-        counter.textContent = newCount + ' out of 5000';
-        // display the thank you message
-        thankYou.style.display = 'block';
-      } else if (data.status === 409) {
-        // the email address already exists in the database
-        // show an alert message and stop the function
-        alert(data.message);
-        return;
-      } else {
-        // some other error occurred
-        // show an alert message and stop the function
-        alert('Something went wrong. Please try again later.');
-        return;
-      }
-    });
-  });  
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://5000twisters:<M3ga0rangeatlas>@twister.tpr4klp.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+app.post('/submit', (req, res) => {
+    const { name, email, options, boxes } = req.body;
+    // Validate email and check for duplicates
+    // Update counter and send submission data to MongoDB
+  });
+  
